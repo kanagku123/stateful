111c07c010882b685e0daacc72e3b33b00be634a
kyungkyuNA <kanagku123@naver.com>
2023-01-10 13:04:04 +0900

    sdfa

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   List<int> numbers = [];
 
+
   void onClick() {
     setState(() { //가독성이 좋기 위해서 setState안에 넣는다.
       numbers.add(numbers.length);
