eb77f10f3186191be99f82115ec749d7984cb94a
kyungkyuNA <kanagku123@naver.com>
2023-01-10 13:02:15 +0900

    sdfa

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyHomePage());
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<int> numbers = [];
+
+  void onClick() {
+    setState(() { //가독성이 좋기 위해서 setState안에 넣는다.
+      numbers.add(numbers.length);
+      print(numbers);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: const Color(0xFFF4EDDB),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Click Count",
+                style: const TextStyle(fontSize: 30),
+              ),
+              for(var n in numbers) Text('$n'),
+              IconButton(
+                onPressed: onClick,
+                icon: const Icon(Icons.add_box_rounded),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
